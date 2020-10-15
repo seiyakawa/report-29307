b5a699f0f215c493e76fd75c5dc29b317964157d
seiyakawa <seiya.kawashima117@gmail.com>
2020-10-15 10:19:06 +0900

    エラーメッセージの表示設定

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,12 +8,20 @@ class ReportsController < ApplicationController
   end
 
   def create
-    Report.create(Report_params)
+    @report = Report.new(report_params)
+   #バリデーションで問題があれば、保存はされず「投稿画面」に戻る
+    if @report.valid?
+      @report.save
+      redirect_to root_path
+    else
+      #保存されなければ、newに戻る
+      render 'new'
+    end
   end
 
   private
 
-  def Report_params
-    params.require(:report).permit(:image, :date, :name, :purpose, :outcome, :action_plan).merge(user_id: current_user.id)
+  def report_params
+    params.require(:report).permit(:image, :date, :name, :purpose, :outcome, :action_plan, :other).merge(user_id: current_user.id)
   end
 end
