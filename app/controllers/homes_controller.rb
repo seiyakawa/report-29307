919826dc76ecac206dc1ea0201d1bfc93b0234cd
seiyakawa <seiya.kawashima117@gmail.com>
2020-10-16 09:11:13 +0900

    ゲストユーザーログイン機能

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,13 @@
+class HomesController < ApplicationController
+  # ゲストユーザーログイン
+  def new_guest
+    # emailの値がすでにあれば、そのユーザーでログイン、データがなければ生成
+    user = User.find_or_create_by!(email: 'aiueo@example.com') do |user|
+      user.nickname = "ゲスト"
+      user.password = 'abc123'
+      user.password_confirmation = 'abc123'
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+end
